e6871bf1a206017f460c37d95d0d38bd0402c62d
store13 <62723206+store13@users.noreply.github.com>
2020-05-08 15:36:08 -0400

    Update LogCall.java

--- src/day_1/LogCall.java
+++ src/day_1/LogCall.java
@@ -3,6 +3,9 @@ package day_1;
 public class LogCall {
     public static void main(String[] args) {
         System.out.println("This class checking the logs");
+        
+        
+        // this line is from Github 
     }
 
 
