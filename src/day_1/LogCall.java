2f6e64e72e9082a2e950eee9c0be7c534aba5511
store13 <torefmly@gmail.com>
2020-05-08 15:40:06 -0400

    add new line

--- src/day_1/LogCall.java
+++ src/day_1/LogCall.java
@@ -3,6 +3,8 @@ package day_1;
 public class LogCall {
     public static void main(String[] args) {
         System.out.println("This class checking the logs");
+
+        // users able to call through class
         
         
         // this line is from Github 
